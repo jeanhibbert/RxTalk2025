f9faf577e395a7813f55b98d20dd1c89417c7f51
jean.hibbert <jean.hibbert@gmail.com>
2025-03-30 21:52:23 +0100

    notes

--- RxConsole/Program.cs
+++ RxConsole/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+//Bart de smet and erik meiyer talk about them
+
+//simplify the syntax
+
+//Javascript and Typescript & python
+
+//async and await integration
